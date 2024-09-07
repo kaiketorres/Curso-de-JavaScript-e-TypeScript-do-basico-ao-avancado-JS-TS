fedc437773f8c002fefcf31b10046808d38dcb88
kaiketorres <kaiketorrea@gmail.com>
2024-09-07 18:10:48 -0300

    Adicionando aula 19 e 20

--- Aula_19/Index.js
+++ Aula_19/Index.js
@@ -1,4 +1,39 @@
-/*Primitivos (imutaveis) - string, number, boolean, undefined, null (bigint, sybol)*/
-let n = 5
-let t = (n === 5 ? console.log('e 5 papai'): console.log('nao e 5 papai') )
+/*Primitivos (imutaveis) - string, number, boolean, undefined, 
+  null (bigint, sybol) - Valor
+*/
+ 
+/*let a = 'A'
+let b = a
 
+console.log(a, b)
+a = 'Outra coisa'
+console.log(a, b)
+*/
+
+/* Referencia (mutavel)  - array, object, function - Passados por referencia*/
+
+/*let a = [1, 2, 3]
+let b = [...a]
+let c = b
+
+console.log(a, b)
+
+a.push(4)
+
+console.log(a, b)
+
+b.pop()
+console.log(a, b)
+a.push('Luiz')
+console.log(c)
+*/
+
+const a = {
+  nome: 'Luiz',
+  sobrenome: 'Otavio'
+}
+
+const b = {...a}
+a.nome = 'kaike'
+
+console.log(a, b)
