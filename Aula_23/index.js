82fe3ed65d8e7dffb39be9b4619db5664d2036d7
kaiketorres <kaiketorrea@gmail.com>
2024-09-08 17:07:00 -0300

    Adicionando aula 23

---
+++ Aula_23/index.js
@@ -0,0 +1,32 @@
+/*
+
+&& -> false && true -> false -> 'o valor mesmo'
+
+|| - > true && false ->
+
+*/
+
+//console.log(0 || false || null || 'Luiz Otavio' || true)
+
+/*
+
+const corUsuario = 'red'
+const corPadrao = corUsuario || 'Preto'
+
+console.log(corPadrao)
+
+*/
+
+
+//console.log('Luiz Otavio' && 0 && 'Maria')
+
+/*const falraOI = () => {
+  return 'oi'
+}
+
+const vaiExecutar = false 
+
+console.log(vaiExecutar && falraOI())
+*/
+
+
