597efa7963ff6bb5a2f147aeded10f1b5ee81abd
kaiketorres <kaiketorrea@gmail.com>
2024-09-09 21:28:14 -0300

    Adicionando aula 25

---
+++ Aula_25/index.js
@@ -0,0 +1,18 @@
+const numero = 5
+
+/*if(numero >= 0 && numero <= 5){
+  console.log('O numero eata entre 0 e 5')
+} else {
+  console.log('Nao esta entre 0 e 5 ')
+}
+*/
+
+if(numero >= 0 && numero <= 5){
+  console.log('O numero eata entre 0 e 5')
+} else if (numero >= 6 && nunero <= 8) {
+  console.log('Nao esta entre 6 e 8')
+} else if(numero >= 9 && numero <= 11){
+  console.log('Nao esta entre 6 e 8')
+} else{
+  console.log('O numero nao esta entre 0 e 11')
+}
