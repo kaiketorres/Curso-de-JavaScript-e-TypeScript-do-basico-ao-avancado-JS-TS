775e9af472e77f30ffd00a6feb779980d0094ef1
kaiketorres <kaiketorrea@gmail.com>
2024-09-22 12:36:01 -0300

    Adicionando aula 30, 31 e 32

---
+++ Aula_32/index.js
@@ -0,0 +1,54 @@
+/*const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]
+
+const [um, dois, tres, ...resto] = numeros ;
+
+console.log(um, dois, tres, resto)
+*/
+
+const numeros = [
+  [1, 2, 3],
+ git a
+  [7, 8, 9]
+];
+
+const [ ,[ , , seis]] = numeros
+
+const [lista1, lista2, lista3] = numeros
+
+console.log(lista2[2])
+console.log(seis)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let a = 'A';
+let b = 'B';
+let c = 'C';
+
+let letras = [b, c, a];
+
+[a, b, c] = letras;
+
+console.log(a, b, c)
+*/
