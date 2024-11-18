ae7ee6efd0b322cf24fa3412dbb29642ad45408b
kaiketorres <kaiketorresdasilva@gmail.com>
2024-11-18 12:41:39 -0300

    update

---
+++ Aula_47/modelo/assets/js/main.js
@@ -0,0 +1,21 @@
+const relogio = document.querySelector('.relogio')
+
+const iniciar = document.querySelector('.iniciar')
+
+const pausar = document.querySelector('.pausar')
+
+const zerar = document.querySelector('.zerar')
+
+iniciar.addEventListener('click', () => {
+  alert('foi')
+})
+
+pausar.addEventListener('click', () => {
+  alert('p')
+})
+
+zerar.addEventListener('click', () => {
+  alert('z')
+})
+
+
